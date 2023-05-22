a0ed5dc2e22e49e36788930fd48b1731b49fdbc5
Siva Sankar Reddy Bogala <sbogala@loginsoft.com>
2023-05-22 21:47:11 +0530

    add config for toolbox

---
+++ cmd/net/ping.go
@@ -0,0 +1,67 @@
+/*
+Copyright © 2023 NAME HERE <EMAIL ADDRESS>
+*/
+package net
+
+import (
+	"fmt"
+	"net/http"
+	"time"
+
+	"github.com/spf13/cobra"
+)
+
+var (
+	url    string
+	client = http.Client{
+		Timeout: 2 * time.Second,
+	}
+)
+
+func ping(domain string) (int, error) {
+	url := "http://" + domain
+	req, err := http.NewRequest("HEAD", url, nil)
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	resp, err := client.Do(req)
+	if err != nil {
+		return 0, err
+
+	}
+	resp.Body.Close()
+	return resp.StatusCode, nil
+}
+
+// pingCmd represents the ping command
+var pingCmd = &cobra.Command{
+	Use:   "ping",
+	Short: "This pings a remote URL and returns the response",
+	Long:  ``,
+	Run: func(cmd *cobra.Command, args []string) {
+		if resp, err := ping(url); err != nil {
+			fmt.Println(err)
+		} else {
+			fmt.Println(resp)
+		}
+	},
+}
+
+func init() {
+	pingCmd.Flags().StringVarP(&url, "url", "u", "", "The url to ping")
+	pingCmd.MarkFlagRequired("url")
+
+	NetCmd.AddCommand(pingCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// pingCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// pingCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
---
+++ main.go
@@ -0,0 +1,10 @@
+/*
+Copyright © 2023 Siva Sankar Redy Bogala
+*/
+package main
+
+import "github.com/shivadharmi/toolbox/cmd"
+
+func main() {
+	cmd.Execute()
+}
